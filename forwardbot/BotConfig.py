32b75b0ef9c9c6e413c1333826ee92a810ec240a
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-04-01 12:03:02 +0530

    Update BotConfig.py

--- forwardbot/BotConfig.py
+++ forwardbot/BotConfig.py
@@ -7,24 +7,18 @@ class Config(object):
     SUDO_USERS = environ.get("SUDO_USERS", "6168162777 6366990600")
     COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "^/")
     HELP_MSG = """
-    💢 **ᴛʜᴇ ᴄᴏᴍᴍᴀɴᴅs ɪɴ ᴛʜᴇ ʙᴏᴛ ᴀʀᴇ:**
+     **Commands**
     
-    🔻 **Command :** /forward
+     **Command :** /forward
     **Usage : ** Forwards messages from a channel to other.
-    🔻 **Command :** /count
+     **Command :** /count
     **Usage : ** Returns the Total message sent using the bot.
-    🔻 **Command :** /reset
+     **Command :** /reset
     **Usage : ** Resets the message count to 0.
-    🔻 **Command :** /restart
+     **Command :** /restart
     **Usage : ** Updates and Restarts the Bot.
-    🔻 **Command :** /join
+     **Command :** /join
     **Usage : ** Joins the channel.
-    🔻 **Command :** /help
-    **Usage : ** Get the help of this bot.
-    🔻 **Command :** /status
-    **Usage :** Check current status of Bot.
-    🔻 **Command :** /uptime
-    **Usage :** Check uptime of Bot.
-    
-    ⭕ **ʙᴏᴛ ɪs ᴄʀᴇᴀᴛᴇᴅ ʙʏ** **@KingVJ01**
+     **Command :** /help
+    **Usage : ** Get the help of this bot
     """
